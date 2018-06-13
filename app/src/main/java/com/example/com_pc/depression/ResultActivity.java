8554452cf869071a981c1950b9c3c7535e5c5e89
SAMUEL-PC\Samuel <samuel.garbo@gmail.com>
2018-06-14 01:25:17 +0900

    ResultActivity: connected firestore to the activity -> results from 3 bdi test -> depression and user data
    calculated depression probability

--- app/src/main/java/com/example/com_pc/depression/ResultActivity.java
+++ app/src/main/java/com/example/com_pc/depression/ResultActivity.java
@@ -1,13 +1,28 @@
 package com.example.com_pc.depression;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.example.com_pc.depression.NaiveBayes.DataObject.DepressionData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
@@ -18,29 +33,165 @@ import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
 public class ResultActivity extends AppCompatActivity {
+    TextView ans1;
+    TextView ans2;
+    TextView ans3;
+    TextView ans4;
+    TextView ans5;
+    TextView ans6;
+    TextView ans7;
+    TextView ans8;
+    TextView ans9;
+    TextView ans10;
+    TextView ans11;
+    TextView ans12;
+    TextView ans13;
+    TextView ans14;
+    TextView ans15;
+    TextView tt1;
 
-final int count=0; //bdi 3번의 점수를 평균 낸 점수
+    FirebaseFirestore mFirestore;
+    double[] scores = new double[3];
+    DepressionData depressionData = new DepressionData();
+
     private GraphicalView mChartView;
 
     private String[] mMonth = new String[]{
             "1차", "2차", "3차"
     };
-    @Override
 
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         Intent intent = getIntent();
+
+         ans1 = findViewById(R.id.ans1);
+         ans2 = findViewById(R.id.ans2);
+         ans3 = findViewById(R.id.ans3);
+         ans4 = findViewById(R.id.ans4);
+         ans5 = findViewById(R.id.ans5);
+         ans6 = findViewById(R.id.ans6);
+         ans7 = findViewById(R.id.ans7);
+         ans8 = findViewById(R.id.ans8);
+         ans9 = findViewById(R.id.ans9);
+         ans10 = findViewById(R.id.ans10);
+         ans11 = findViewById(R.id.ans11);
+         ans12 = findViewById(R.id.ans12);
+         ans13 = findViewById(R.id.ans13);
+         ans14 = findViewById(R.id.ans14);
+         ans15 = findViewById(R.id.ans15);
+         tt1 = findViewById(R.id.tt1);
+
         TextView tt1 = (TextView)findViewById(R.id.tt0);  //수정
         tt1.setText(intent.getStringExtra("Title"));
         // tt2.setText(intent.getStringExtra("date"));
-        if(count<=9)
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+        final DocumentReference docDepressionData =mFirestore.collection("Userdata").document("data");
+        final DocumentReference docUserData = mFirestore.collection("Users").document("1");
+
+        final CollectionReference colBDI1 = mFirestore.collection("Users").document("1").collection("BDI1");
+        final CollectionReference colBDI2 = mFirestore.collection("Users").document("2").collection("BDI2");
+        final CollectionReference colBDI3 = mFirestore.collection("Users").document("3").collection("BDI3");
+
+        final Query queryBDI1 = colBDI1.orderBy("date", Query.Direction.DESCENDING).limit(1);
+        final Query queryBDI2 = colBDI2.orderBy("date", Query.Direction.DESCENDING).limit(1);
+        final Query queryBDI3 = colBDI3.orderBy("date", Query.Direction.DESCENDING).limit(1);
+
+        // Fetch BDI1 result
+        queryBDI1.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        {
+                            scores[0] = document.getDouble("score");
+                        }
+                    }
+                }
+            }
+        });
+        // Fetch BDI2 result
+        queryBDI2.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        {
+                            scores[1] = document.getDouble("score");
+                        }
+                    }
+                }
+            }
+        });
+        // Fetch BDI3 result
+        queryBDI3.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        {
+                            scores[2] = document.getDouble("score");
+                        }
+                    }
+                }
+            }
+        });
+
+        docUserData.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                depressionData = documentSnapshot.toObject(DepressionData.class);
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+            }
+        });
+
+        docDepressionData.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                depressionData = documentSnapshot.toObject(DepressionData.class);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+            }
+        });
+        ans1.setText(String.valueOf(depressionData.getAge()));
+        ans2.setText(String.valueOf(depressionData.getGender()));
+        ans3.setText(String.valueOf(depressionData.isSmoking()));
+        ans4.setText(String.valueOf(depressionData.isAlcohol()));
+        ans5.setText(String.valueOf(depressionData.isTroubleWithClassmates()));
+        ans6.setText(String.valueOf(depressionData.isLoosingFriend()));
+        ans7.setText(String.valueOf(depressionData.isTroubleWithTeacher()));
+        ans8.setText(String.valueOf(depressionData.isTroubleWithSiblings()));
+        ans9.setText(String.valueOf(depressionData.isArguingWithParents()));
+        ans10.setText(String.valueOf(depressionData.isParentsArguing()));
+        ans11.setText(String.valueOf(depressionData.isBreakup()));
+        ans12.setText(String.valueOf(depressionData.isSeriouslyIll()));
+        ans13.setText(String.valueOf(depressionData.isFamilyMemberIll()));
+        ans14.setText(String.valueOf(depressionData.getParentsMaritalStatus()));
+        ans15.setText(String.valueOf(depressionData.getLivingStandard()));
+
+        double dep = depressionData.depProbability();
+        double notDep = depressionData.notDepProbability();
+        double depProbability = dep / (dep + notDep);
+        double notDepProbability = notDep / (dep + notDep);
+        tt1.setText("사용자가 우울증에 걸릴 확률은 "+String.valueOf(depProbability) +", 우울증에 걸리지 않을 확률은 "+String.valueOf(notDepProbability));
+
+
+        if(scores[2]<=9)
             tt1.setText("우울하지 않은 상태입니다.");
-        else if(count>=10&&count<=15) {
+        else if(scores[2]>=10&&scores[2]<=15) {
             tt1.setText("가벼운 우울 상태입니다.");
         }
-        else if(count>=16&&count<=23) {
-            tt1.setText("가벼운 우울 상태입니다.");
+        else if(scores[2]>=16&&scores[2]<=23) {
+            tt1.setText("중한 우울 상태입니다.");
         }
         else
             tt1.setText("심각한 우울 상태입니다."); // 여기까지 ! (마지막 BDI 진단 값을 받아서 출력해주어야해요!)
@@ -51,7 +202,7 @@ final int count=0; //bdi 3번의 점수를 평균 낸 점수
     private void drawChart() { // drawChart 함수는 모두 복사해서 붙여넣기 해주시면 될거 같아요~
 
         int[] x = {0, 1, 2};
-        int[] income = {40, 20, 35};
+        int[] income = {(int)scores[0], (int)scores[1], (int)scores[2]};
         XYSeries incomeSeries = new XYSeries("");
         // Creating an XYSeries for Expense
         XYSeries expenseSeries = new XYSeries("");
